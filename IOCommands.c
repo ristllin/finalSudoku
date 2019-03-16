bfabd41ac821d337c340844b635e7c44d848ae99
Shahar Eshed <shahare@lan.wixpress.com>
2019-03-16 17:22:17 +0200

    commands - update function signtures, Main - add to excute pointer to current posiyion in the list, IOCommands - nada

--- IOCommands.c
+++ IOCommands.c
@@ -5,7 +5,20 @@
  *      Author: shahare
  */
 
-void printBoard(int* board){
+void printBoard(int* board, int* state){
+	/*function description: print the board
+	 * state: Solve, Edit
+	 * args:
+	 * return: void
+	 */
+
+}
+
+void readBoardFromFile(???){
+
+}
+
+void writeBoardToFile(???) {
 
 }
 
