c11b37a547310bdd11e115f5b8554c69c3c39f8c
Shahar Eshed <shahare@lan.wixpress.com>
2019-03-16 14:50:32 +0200

    skeleton basics

---
+++ Commands.h
@@ -0,0 +1,13 @@
+/*
+ * Commands.h
+ *
+ *  Created on: Mar 16, 2019
+ *      Author: shahare
+ */
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+
+
+#endif /* COMMANDS_H_ */
---
+++ IOCommands.h
@@ -0,0 +1,13 @@
+/*
+ * Print.h
+ *
+ *  Created on: Mar 16, 2019
+ *      Author: shahare
+ */
+
+#ifndef IOCOMMANDS_H_
+#define IOCOMMANDS_H_
+
+
+
+#endif /* IOCOMMANDS_H_ */
