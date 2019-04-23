628c21727f565c4d6081a0d35839e0071e033867
unknown <ristlin@gmail.com>
2019-04-23 18:42:31 +0300

    added is_legal, options for cell and is_finished

--- AuxFunctions.c
+++ AuxFunctions.c
@@ -10,14 +10,73 @@ int is_finished(int n, int m, int* board){
 	 * args: board - sudoku board n*n
 	 * return: 0 - didn't win, 1 - won.
 	 */
-	return 0;
+	int x,y,location = 0;
+	const N = n * m;
+	for (x=0;x<N;x++){
+		for (y=0;y<N;y++){
+			location = (x + y*N)*2;
+			if (!isLegal(n,m,x,y,board[location] || board[location] == 0)){return 0;} /*not legal or empty*/
+		}
+	}
+	return 1;
 }
 
-void options_for_location(int x,int y, int n, int m, int* board, int* options){
-	/*function description: returns an int array of length n with all of the legal options for a specific (x,y) slot in the board
-	 * args: x,y, - location to check options in; n,m,board - sudoku board, options - array to fill with options
-	 * return: options - int array, 0- not a legal option, 1- a legal option for the location
+int optionForLocation(int n, int m, int x, int y, int* board, int* legal_options){
+	/*function description: returns which options are legal for cell (x,y) {starting from 0} in the given board.
+	 * using N integers in an array, stating with 1\0, according to their place, whether or not a number is legal to that position.
+	 * args: legal options, an N long array, "calloced" in advance!
+	 * return: 1 current value in position is legal, 0 if illegal (and legal options is not valid). returns legal if empty.
 	 */
+	int i,j,location,tmpx,tmpy; int N = n*m; int* temp_legal;
+	temp_legal = (int*)calloc(N,sizeof(int));
+	for (i=0;i<N;i++){ /*zero out N variables in options list*/
+		legal_options[i] = 1;
+		temp_legal[i] = 1;
+	}
+	if (x > N || y > N){printf("<<<isLegal() assertion - illegal parameters x or y>>>\n"); return 0;} /*assertion*/
+	for (i=0;i<N;i++){ /*check row*/
+		location = ((y*N)+i)*2; /*y*N*2 - starting row location on board*/
+		if (i == x || board[location] == 0){continue;} /*skip x,y cell and 0s*/
+		if (temp_legal[board[location]-1] == 0){return 0;} /*double value -1 because temp_legal counts from 0*/
+		temp_legal[board[location]-1] = 0;
+		legal_options[board[location]-1] = 0;
+	}
+	for (i=0;i<=N;i++){temp_legal[i] = 1;} /*zero temp board*/
+	for (i=0;i<N;i++){ /*check column*/
+		location = ((i*N)+x)*2; /*i*N*2+x - starting row location on board*/
+		if (i == y || board[location] == 0){continue;} /*skip x,y cell and 0s*/
+		if (temp_legal[board[location]-1] == 0){return 0;} /*double value - -1 because temp_legal counts from 0*/
+		temp_legal[board[location]-1] = 0;
+		legal_options[board[location]-1] = 0;
+	}
+	for (i=0;i<N;i++){temp_legal[i] = 1;} /*zero temp board*/
+	for (i=0;i<N;i++){ /*check box*/
+		tmpx = (x/n)*n+i%n;
+		tmpy = (y/m)*m+i/n;
+		location = (tmpx+tmpy*N)*2;
+		if ((tmpx == x && tmpy == y) || board[location] == 0){continue;} /*skip x,y cell and 0s*/
+		if (temp_legal[board[location]-1] == 0){return 0;}
+		temp_legal[board[location]-1] = 0;
+		legal_options[board[location]-1] = 0;
+	}
+	free(temp_legal);
+	return 1; /*finished successfully*/
+}
+
+int is_legal(int n, int m, int x, int y, int* board){
+	/*function description: Uses optionForLocation. checks if current value in cell is legal.
+	 * args:
+	 * return: 1 if current value in cell is legal; 0 if not or if illegal state found in row\column\box of the cell. returns legal (1) for empty cell.
+	 */
+	int* legal_options; int rslt,location; const N = n*m;
+	legal_options = calloc(N,sizeof(int));
+	location = (x+y*N)*2;
+	if (board[location] == 0){return 1;} /*empty cell*/
+	rslt = optionForLocation(n,m,x,y,board,legal_options);
+	if (rslt == 0){return 0;}
+	if (legal_options[board[location]-1] == 0){return 0;} /*illegal option for cell*/
+	free(legal_options);
+	return 1; /*valid option*/
 }
 
 void seperator(int n,int m){
